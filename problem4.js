bfce95d54521a240cecf42557e3079a9daeb699d
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-05-18 18:08:57 +0600

    create repo and add to remote

--- problem4.js
+++ problem4.js
@@ -2,6 +2,7 @@
 
 `Create An Array Of Objects Representing Cars With Properties Like Make, Model, And Year. Write A Function To Sort The Array Of Cars By The Year Of Manufacture In Ascending Order. Print The Sorted Array`;
 
+
 const cars = [
   { make: "Chevrolet", model: "Silverado", year: 2017 },
   { make: "Toyota", model: "Camry", year: 2020 },
